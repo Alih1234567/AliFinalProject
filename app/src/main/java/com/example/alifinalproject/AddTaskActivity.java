cb310fdeb8f913c0a46d5246c3762bf138684967
hanan <hnan.akawi@gmail.com>
2024-12-04 11:31:00 +0200

    fdthgdfg

--- app/src/main/java/com/example/alifinalproject/AddTaskActivity.java
+++ app/src/main/java/com/example/alifinalproject/AddTaskActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AddTaskActivity extends AppCompatActivity {
-
+//ghjgfhjgfhjf
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
