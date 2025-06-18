234d679530f1fe433a81e0e42b5d64a10d98f709
USER <hasarmi.ali@gmail.com>
2025-06-18 19:07:37 +0300

    kkjkj

--- app/src/main/java/com/example/alifinalproject/SignUpActivity.java
+++ app/src/main/java/com/example/alifinalproject/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.example.alifinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -18,54 +21,62 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
-    private TextInputEditText etEmail, etPassword;
+    private EditText etEmail, etPassword;
+
     private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
 
-        etEmail = findViewById(R.id.etEmail);
-        etPassword = findViewById(R.id.etPassword);
-        btnSignUp = findViewById(R.id.btnSignIn);
+//        etEmail = findViewById(R.id.etEmail);
+//        etPassword = findViewById(R.id.etPassword);
+//
+//        btnSignUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+//            }
+//        });
+        
     }
-        private void checkEmailPassword()
-        {
-            boolean isALLOK = true;
-            String email = etEmail.getText().toString();
-            String password = etPassword.getText().toString();
-            if (email.length() < 6 || email.contains("@") == false) {
-                isALLOK = false;
-                etEmail.setError("Wrong Email");
-            }
-            if (password.length() < 8 || password.contains("") == true) {
-                isALLOK = false;
-                etPassword.setError("Wrong Password");
-            }
-            if (isALLOK) {
-                //كائن لعملية التسجبل
-                FirebaseAuth auth= FirebaseAuth.getInstance();
-                //انشاء حساب بواسطة ايميل وسيسما
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override//התגובה שמתקבל הניסיון הרישום בענן
-                    public void onComplete(@NonNull Task<AuthResult> task) {//הפרמטר מכיל מידע מהשרת על תוצאת הבקשה לרישום
-                        if(task.isSuccessful()){//اذا كانت عملية التسجيل ناجحة
-                            Toast.makeText(SignUpActivity.this, "Signing up Succeeded", Toast.LENGTH_SHORT).show();
-                            finish();
-                        }
-                        else{
-                            Toast.makeText(SignUpActivity.this, "Signing up Failed", Toast.LENGTH_SHORT).show();
-                            etEmail.setError(task.getException().getMessage());//عرض الرسالة الخاصة بالخطأ
-                        }
-
-                    }
-                });
-
-
-            }
-
-
-        }
+//        private void checkEmailPassword()
+//        {
+//            boolean isALLOK = true;
+//            String email = etEmail.getText().toString();
+//            String password = etPassword.getText().toString();
+//            if (email.length() < 6 || email.contains("@") == false) {
+//                isALLOK = false;
+//                etEmail.setError("Wrong Email");
+//            }
+//            if (password.length() < 8 || password.contains("") == true) {
+//                isALLOK = false;
+//                etPassword.setError("Wrong Password");
+//            }
+//            if (isALLOK) {
+//                //كائن لعملية التسجبل
+//                FirebaseAuth auth= FirebaseAuth.getInstance();
+//                //انشاء حساب بواسطة ايميل وسيسما
+//                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override//התגובה שמתקבל הניסיון הרישום בענן
+//                    public void onComplete(@NonNull Task<AuthResult> task) {//הפרמטר מכיל מידע מהשרת על תוצאת הבקשה לרישום
+//                        if(task.isSuccessful()){//اذا كانت عملية التسجيل ناجحة
+//                            Toast.makeText(SignUpActivity.this, "Signing up Succeeded", Toast.LENGTH_SHORT).show();
+//                            finish();
+//                        }
+//                        else{
+//                            Toast.makeText(SignUpActivity.this, "Signing up Failed", Toast.LENGTH_SHORT).show();
+//                            etEmail.setError(task.getException().getMessage());//عرض الرسالة الخاصة بالخطأ
+//                        }
+//
+//                    }
+//                });
+//
+//
+//            }
+//
+//
+//        }
     }
